57d593818f01b5037ecd3de786d001465fb792b4
Josué Sánchez Domínguez <joss00314@gmail.com>
2025-10-06 14:11:14 -0600

    feat: Initial laravel 12 project with database setup

---
+++ app/Http/Controllers/HealthCheckController.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+* @developer Josué Sánchez Domínguez
+* @date_created 06/10/2025
+* @origin_desc Archivo base generado automáticamente por artisan
+* make:controller
+** @modification_desc Se agregó un Controlador prueba 
+* @date_modified 06/10/2025
+* @folio_mod FOL-001
+*
+* @authorized_by 
+* @tested_by 
+* @released_by 
+*
+* @version 1.1
+*/
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HealthCheckController extends Controller
+{
+    //
+}
